006dd9f4df947cf1cfa09b22e7afe49f067f1c40
bingweibi <794654932@qq.com>
2017-06-23 16:01:38 +0800

    update

---
+++ chapter3/UILayoutTest/app/src/main/java/com/example/bbw/uilayouttest/FrameLayout.java
@@ -0,0 +1,13 @@
+package com.example.bbw.uilayouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+    }
+}
